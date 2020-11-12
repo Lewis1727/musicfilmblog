8bee9100fb22ff7e1bcdb76550cd17d482085850
Lewis1727 <59251780+Lewis1727@users.noreply.github.com>
2020-11-12 14:29:37 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 	session_start();
 	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 	//$conn = mysqli_connect("localhost", "lewis", "!Monopolist1344", "PHP_BLOG");
-	$conn = mysqli_connect("localhost", "root", "!Monopolist1344", "PHP_BLOG");
+	$conn = mysqli_connect("localhost", "root", "", "PHP_BLOG");
 
 	if (!$conn) {
 		die("Error connecting to database: " . mysqli_connect_error());
