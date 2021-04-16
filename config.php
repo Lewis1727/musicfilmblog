b7f3109203bef155f90b95b23fb03e1f9a4a056a
Lewis1727 <59251780+Lewis1727@users.noreply.github.com>
2021-04-16 16:33:01 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
 	session_start();
 	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-	//$conn = mysqli_connect("localhost", "lewis", "!Monopolist1344", "PHP_BLOG");
+	//$conn = mysqli_connect("localhost", "root", "", "PHP_BLOG");
 	$conn = mysqli_connect("localhost", "root", "", "PHP_BLOG");
 
 	if (!$conn) {
